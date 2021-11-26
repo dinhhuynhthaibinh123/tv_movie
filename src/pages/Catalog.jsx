3ca565aca78f14694ba7888032609418e646ac25
ChovyDoinb <binhdinhqt137@gmail.com>
2021-11-27 01:28:26 +0700

    Movie-list, card

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
+import { useParams } from "react-router";
 const Catalog = () => {
+  const { category } = useParams();
+
+
   return <div>Catalog</div>;
 };
 
 export default Catalog;
- 
